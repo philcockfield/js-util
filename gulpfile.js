cccd42f943759fe5ad90cdad99bc73af31c15b0a
Phil Cockfield <phil@cockfield.net>
2015-07-04 20:07:29 -0700

    Factor out 'build-util' into it's own 'build' gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 
-gulp.task('default', function () {
+gulp.task('build', ['build-util']);
+gulp.task('build-util', function () {
   return gulp.src('./util/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/util'));
 });
+
+
+// ----------------------------------------------------------------------------
+gulp.task('default', ['build']);
