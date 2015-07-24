5f6607a151e44e032497345385abec46a2adb204
Phil Cockfield <phil@cockfield.net>
2015-07-24 16:00:01 +1200

    Remove `dist`.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,7 @@
 'use strict'
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var concat = require('gulp-concat');
-var babel = require('gulp-babel');
 var SOURCE_PATH = './src/**/*.js';
-
-
-gulp.task('build', function () {
-  return gulp.src(SOURCE_PATH)
-    .pipe(babel())
-    // .pipe(concat('util.js'))
-    .pipe(gulp.dest('dist'));
-});
-gulp.task('watch', function(callback) { gulp.watch(SOURCE_PATH, ['build']) });
-
 
 
 gulp.task('lint', function() {
@@ -23,5 +11,4 @@ gulp.task('lint', function() {
 });
 
 
-// ----------------------------------------------------------------------------
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['lint']);
