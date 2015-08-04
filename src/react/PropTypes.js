1583987ef74ec35ac1ce0f2c363c488e666d3aba
Phil Cockfield <phil@cockfield.net>
2015-08-05 05:47:50 +1200

    NUMBER_OR_STRING constant.

--- src/react/PropTypes.js
+++ src/react/PropTypes.js
@@ -1,13 +1,18 @@
 import _ from "lodash";
 import React from "react";
 
+const PropTypes = _.clone(React.PropTypes);
+PropTypes.NUMBER_OR_STRING = PropTypes.oneOfType([
+  React.PropTypes.number,
+  React.PropTypes.string
+]);
+
+
 /**
  * Store the parameter values on the returned
  * validator functions so that external tools
  * (such the UIHarness) can display the values.
  */
-const PropTypes = _.clone(React.PropTypes);
-
 
 PropTypes.oneOf = (enumValues) => {
   const result = React.PropTypes.oneOf(enumValues);
