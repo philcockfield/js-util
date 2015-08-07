5d757abf2db6fd5b7ebaca67d34a10034575972a
Phil Cockfield <phil@cockfield.net>
2015-08-07 17:55:01 +1200

    PropTypes - toString conversions.

--- src/react/PropTypes.js
+++ src/react/PropTypes.js
@@ -4,6 +4,7 @@ import Validator from "./Validator";
 const PropTypes = _.clone(React.PropTypes);
 
 
+
 /**
  * Common combinations of types.
  */
@@ -34,21 +35,81 @@ PropTypes.validate = (propTypes, props, componentName) => {
  * validator functions so that external tools
  * (such the UIHarness) can display the values.
  */
-
 PropTypes.oneOf = (enumValues) => {
   const result = React.PropTypes.oneOf(enumValues);
-  result.oneOf = enumValues;
-  result.isRequired.oneOf = enumValues;
+  const decorate = (obj) => {
+        obj.oneOf = enumValues;
+        obj.toString = () => {
+            return `oneOf(${ enumValues.join(", ") })`;
+          };
+      };
+  decorate(result);
+  decorate(result.isRequired);
+  return result;
+};
+
+
+PropTypes.oneOfType = (types) => {
+  const result = React.PropTypes.oneOfType(types);
+  const decorate = (obj) => {
+        obj.oneOfType = types;
+        obj.toString = () => {
+            const typeNames = _.map(types, type => type.toString()).join(", ");
+            return `oneOfType(${ typeNames })`;
+          };
+      };
+  decorate(result);
+  decorate(result.isRequired);
+  return result;
+};
+
+
+const shapeToObject = (obj) => {
+  let result = {};
+  _.keys(obj).forEach(key => {
+        let value = obj[key];
+        if (_.isFunction(value)) {
+          result[key] = `<${ value.type || "unknown" }>`;
+        }
+        if (_.isPlainObject(value)) {
+          result[key] = shapeToObject(value); // <== RECURSION.
+        }
+      });
   return result;
 };
 
 
 PropTypes.shape = (shape) => {
   const result = React.PropTypes.shape(shape);
-  result.shape = shape;
-  result.isRequired.shape = shape;
+  const decorate = (obj) => {
+        obj.shape = shape;
+        obj.toString = () => {
+            let output = shapeToObject(shape);
+            output = JSON.stringify(output).replace(/\"/g, "");
+            return `shape(${ output })`;
+          };
+      };
+  decorate(result);
+  decorate(result.isRequired);
   return result;
 };
 
 
+// ----------------------------------------------------------------------------
+
+
+// Store type names.
+_.keys(PropTypes).forEach(key => {
+      const prop = PropTypes[key];
+      prop.type = key;
+      prop.toString = () => {
+          const overrides = ["oneOf", "oneOfType", "shape"];
+          return _.has(overrides, key)
+              ? prop.toString()
+              : key;
+        };
+    });
+
+
+// ----------------------------------------------------------------------------
 export default PropTypes;
--- test/react/PropTypes.test.js
+++ test/react/PropTypes.test.js
@@ -1,9 +1,10 @@
+import React from "react";
 import { expect } from "chai";
 import { PropTypes } from "../../src/react";
 
 
 
-describe("React", () => {
+describe("React PropTypes", () => {
   describe("PropTypes.oneOf", () => {
     it("stores enum values on return object", () => {
       const result = PropTypes.oneOf(['one', 'two']);
@@ -13,6 +14,29 @@ describe("React", () => {
     it("stores enum values on the corresponding `isRequired` object", () => {
       const result = PropTypes.oneOf(['one', 'two']);
       expect(result.isRequired.oneOf).to.eql(['one', 'two']);
+    });
+
+    it("converts toString()", () => {
+      const result = PropTypes.oneOf(['one', 'two']);
+      expect(result.toString()).to.equal("oneOf(one, two)");
+    });
+  });
+
+
+  describe("PropTypes.oneOfType", () => {
+    it("stores type values on return object", () => {
+      const result = PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]);
+      expect(result.oneOfType).to.eql([React.PropTypes.string, React.PropTypes.number]);
+    });
+
+    it("stores type values on the corresponding `isRequired` object", () => {
+      const result = PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]);
+      expect(result.isRequired.oneOfType).to.eql([React.PropTypes.string, React.PropTypes.number]);
+    });
+
+    it("converts toString()", () => {
+      const result = PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]);
+      expect(result.toString()).to.equal("oneOfType(string, number)");
     });
   });
 
@@ -26,6 +50,16 @@ describe("React", () => {
     it("stores the shape on the corresponding `isRequired` object", () => {
       const result = PropTypes.shape({ isEnabled: PropTypes.bool });
       expect(result.isRequired.shape).to.eql({ isEnabled: PropTypes.bool });
+    });
+
+    it("converts toString()", () => {
+      const result = PropTypes.shape({
+        isEnabled: PropTypes.bool,
+        foo: {
+          total: React.PropTypes.number,
+        }
+      });
+      expect(result.toString()).to.equal("shape({isEnabled:<bool>,foo:{total:<number>}})");
     });
   });
 });
